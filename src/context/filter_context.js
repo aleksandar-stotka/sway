15a4e4a636ce0812b2c63d3ad4c21e00ba1c48c2
aleksandar angelevski <stotka123@yahoo.com>
2021-11-05 13:35:12 +0100

    start-filters

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -16,7 +16,17 @@ const initialState = {
   filtered_products: [],
   all_products: [],
   grid_view: true,
-  sort:'price-lowest'
+  sort:'price-lowest',
+  filters: {
+    text: '',
+    company: 'all',
+    category: 'all',
+    color: 'all',
+    min_price: 0,
+    max_price: 0,
+    price:0
+
+  }
 };
 ////////////////////////////////////////////////////
 
@@ -28,6 +38,10 @@ export const FilterProvider = ({ children }) => {
   useEffect(() => {
     dispatch({ type: LOAD_PRODUCTS, payload: products });
   }, [products]);
+
+  useEffect(()=> {
+             dispatch({type: SORT_PRODUCTS})
+  },[products,state.sort])
 
   const setGridView = () => {
     dispatch({type: SET_GRIDVIEW})
