7eb8d2c862968b7ca1adbd6d853cc28f49a521d2
aleksandar angelevski <stotka123@yahoo.com>
2021-11-08 14:42:33 +0100

    setCart-setup

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -7,7 +7,24 @@ import {
 } from "../actions";
 
 const cart_reducer = (state, action) => {
-  return state;
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload;
+    const tempItem = state.cart.find((i) => i.id === id + color);
+    if (tempItem) {
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      };
+      return { ...state, cart: [...state.cart, newItem] };
+    }
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
