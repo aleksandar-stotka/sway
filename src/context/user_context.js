328431218dd315b976e09786a29b843d73fbbb18
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-08-03 11:44:14 -0700

    delete-user

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -3,7 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const UserContext = React.createContext();
 export const UserProvider = ({ children }) => {
-  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
+  const { isAuthenticated, loginWithRedirect, logout } =
     useAuth0();
   const [myUser, setMyUser] = useState(null);
 
